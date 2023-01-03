bb9051c35fa0d81c9a7da91dbfad46574af4b435
chosungwoo <ejyoo12345@naver.com>
2023-01-03 09:49:24 +0900

    콜리젼 메트릭스, 애니메이터 스피드 수정

--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -34,6 +34,18 @@ public class PlayerControl : MonoBehaviour
     {
         // Create waitforfixed update for use in coroutine
         waitForFixedUpdate = new WaitForFixedUpdate();
+
+        // Set player animation speed
+        SetPlayerAnimationSpeed();
+    }
+
+    /// <summary>
+    /// Set player animator speed to match movement speed
+    /// </summary>
+    private void SetPlayerAnimationSpeed()
+    {
+        // Set animator speed to match movement speed
+        player.anim.speed = moveSpeed / Settings.baseSpeedForPlayerAnimations;
     }
 
     private void Update()
