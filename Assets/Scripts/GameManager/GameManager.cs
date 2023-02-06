2a81e690d553069c4124bd73d3980be46cc0337b
Chosungwoo123 <ejyoo12345@naver.com>
2023-02-06 20:20:29 +0900

    미니맵 스크립트 작성

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -154,6 +154,14 @@ public class GameManager : SingletonMonoBehaviour<GameManager>
     }
 
     /// <summary>
+    /// Get the player minimap icon
+    /// </summary>
+    public Sprite GetPlayerMiniMapIcon()
+    {
+        return playerDetails.playerMiniMapIcon;
+    }
+
+    /// <summary>
     /// Get the current room the player is in
     /// </summary>
     public Room GetCurrentRoom()
