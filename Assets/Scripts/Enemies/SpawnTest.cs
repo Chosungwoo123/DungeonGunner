7541e26288d717132559e060ae3ca99b298b896e
Chosungwoo123 <ejyoo12345@naver.com>
2023-07-01 00:00:07 +0900

    모든 방에 몬스터 관련 데이터 추가, 몬스터 생성 파라미터 구현

--- Assets/Scripts/Enemies/SpawnTest.cs
+++ Assets/Scripts/Enemies/SpawnTest.cs
@@ -1,36 +1,59 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnTest : MonoBehaviour
 {
-    public RoomTemplateSO roomTemplateSO;
     private List<SpawnableObjectsByLevel<EnemyDetailsSO>> testLevelSpawnList;
     private RandomSpawnableObject<EnemyDetailsSO> randomEnemyHelperClass;
-    private GameObject instantiatedEnemy;
+    private List<GameObject> instantiatedEnemyList = new List<GameObject>();
 
-    private void Start()
+    private void OnEnable()
     {
-        // Get test level spawn list from dungeon room template
-        testLevelSpawnList = roomTemplateSO.enemiesByLevelList;
-
-        // Create RandomSpawnableObject helper class
-        randomEnemyHelperClass = new RandomSpawnableObject<EnemyDetailsSO>(testLevelSpawnList);
+        // Subscribe to change of room
+        StaticEventHandler.OnRoomChanged += StaticEventHandler_OnRoomChanged;
     }
 
+    private void OnDisable()
+    {
+        // Unsbscribe to change of room
+        StaticEventHandler.OnRoomChanged -= StaticEventHandler_OnRoomChanged;
+    }
+
+    private void StaticEventHandler_OnRoomChanged(RoomChangeEventArgs roomChangeEventArgs)
+    {
+        // Destroy any spawned enemies
+        if (instantiatedEnemyList != null && instantiatedEnemyList.Count > 0)
+        {
+            foreach (GameObject enemy in instantiatedEnemyList)
+            {
+                Destroy(enemy);
+            }
+        }
+
+        RoomTemplateSO roomTemplateSo = DungeonBuilder.Instance.GetRoomTemplate(roomChangeEventArgs.room.templateID);
+
+        if (roomTemplateSo != null)
+        {
+            // Get test level spawn list from dungeon room template
+            testLevelSpawnList = roomTemplateSo.enemiesByLevelList;
+            
+            // Create RandomSpawnableObject helper class
+            randomEnemyHelperClass = new RandomSpawnableObject<EnemyDetailsSO>(testLevelSpawnList);
+        }
+    }
+    
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.T))
         {
-            if (instantiatedEnemy != null)
-            {
-                Destroy(instantiatedEnemy);
-            }
-
             EnemyDetailsSO enemyDetails = randomEnemyHelperClass.GetItem();
 
             if (enemyDetails != null)
             {
-                instantiatedEnemy = Instantiate(enemyDetails.enemyPrefab, HelperUtilities.GetSpawnPositionNearestToPlayer(HelperUtilities.GetMouseWorldPosition()), Quaternion.identity);
+                instantiatedEnemyList.Add(Instantiate(enemyDetails.enemyPrefab,
+                    HelperUtilities.GetSpawnPositionNearestToPlayer(HelperUtilities.GetMouseWorldPosition()),
+                    Quaternion.identity));
             }
         }
     }
