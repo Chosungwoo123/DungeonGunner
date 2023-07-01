6a1811b1344415e2cf8e54ac01d31c14b69fd049
Chosungwoo123 <ejyoo12345@naver.com>
2023-07-02 03:18:02 +0900

    적 생성될때 특수효과 추가

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -3,18 +3,21 @@ using UnityEngine;
 using UnityEngine.Rendering;
 
 #region REQUIRE CONPONENTS
+
 [RequireComponent(typeof(EnemyMovementAI))]
 [RequireComponent(typeof(MovementToPositionEvent))]
 [RequireComponent(typeof(MovementToPosition))]
 [RequireComponent(typeof(IdleEvent))]
 [RequireComponent(typeof(Idle))]
 [RequireComponent(typeof(AnimateEnemy))]
+[RequireComponent(typeof(MaterializeEffect))]
 [RequireComponent(typeof(SortingGroup))]
 [RequireComponent(typeof(SpriteRenderer))]
 [RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(CircleCollider2D))]
 [RequireComponent(typeof(PolygonCollider2D))]
+
 #endregion
 
 [DisallowMultipleComponent]
@@ -27,6 +30,7 @@ public class Enemy : MonoBehaviour
     [HideInInspector] public Animator anim;
 
     private EnemyMovementAI enemyMovementAI;
+    private MaterializeEffect materializeEffect;
     private CircleCollider2D circleCollider2D;
     private PolygonCollider2D polygonCollider2D;
 
@@ -36,6 +40,7 @@ public class Enemy : MonoBehaviour
         enemyMovementAI = GetComponent<EnemyMovementAI>();
         movementToPositionEvent = GetComponent<MovementToPositionEvent>();
         idleEvent = GetComponent<IdleEvent>();
+        materializeEffect = GetComponent<MaterializeEffect>();
         circleCollider2D = GetComponent<CircleCollider2D>();
         polygonCollider2D = GetComponent<PolygonCollider2D>();
         spriteRendererArray = GetComponentsInChildren<SpriteRenderer>();
@@ -52,6 +57,9 @@ public class Enemy : MonoBehaviour
         SetEnemyMovementUpdateFrame(enemySpawnNumber);
 
         SetEnemyAnimationSpeed();
+
+        // Materialize enemy
+        StartCoroutine(MaterializeEnemy());
     }
 
     /// <summary>
@@ -70,5 +78,28 @@ public class Enemy : MonoBehaviour
     {
         // Set animator speed to match movement speed
         anim.speed = enemyMovementAI.moveSpeed / Settings.baseSpeedForEnemyAnimations;
+    }
+
+    private IEnumerator MaterializeEnemy()
+    {
+        // Disable collider, Movement AI and weapon AI
+        EnemyEnable(false);
+
+        yield return StartCoroutine(materializeEffect.MaterializeRoutine(enemyDetails.enemyMaterializeShader,
+            enemyDetails.enemyMaterializeColor, enemyDetails.enemyMaterializeTime, spriteRendererArray,
+            enemyDetails.enemyStandardMaterial));
+
+        // Enable collider, Movement AI and weapon AI
+        EnemyEnable(true);
+    }
+
+    private void EnemyEnable(bool isEnabled)
+    {
+        // Enable/Disable colliders
+        circleCollider2D.enabled = isEnabled;
+        polygonCollider2D.enabled = isEnabled;
+        
+        // Enable/Disable movement AI
+        enemyMovementAI.enabled = isEnabled;
     }
 }
